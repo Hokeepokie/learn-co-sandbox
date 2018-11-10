6e56c4110438a9bc05c84a51b836da808f1ea55e
Eliza Cheung <elizacheung7@gmail.com>
2018-11-10 20:25:17 +0000

    Done.

---
+++ counters.rb
@@ -0,0 +1,9 @@
+
+n=1
+
+101.times do |n|
+  if n != 0
+    puts n
+  n =+ 1
+end
+end
---
+++ hello_world.rb
@@ -0,0 +1,16 @@
+def hello (name)
+  puts "Hello #{name}"
+end
+
+name = "Eliza"
+
+hello(name)
+
+
+
+
+def add (arg1, arg2)
+  return arg1 + arg2
+end
+
+add (1,1)
