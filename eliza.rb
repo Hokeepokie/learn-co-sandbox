4366dcc7344db8dbf46f78adc84b668f367612c7
Eliza Cheung <elizacheung7@gmail.com>
2018-11-11 19:00:22 +0000

    Done.

---
+++ eliza.rb
@@ -0,0 +1,13 @@
+(1..100).each do |thing|
+if thing %3 == 0 
+  puts thing " Fizz"
+else if
+  thing %5 == 0 
+  puts thing " Buzz"
+else if
+  thing %5 ==0 && thing %3 == 0 
+  puts thing "Fizz Buzz"
+end
+end
+end
+
